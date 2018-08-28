3500de1caa8dcd632e270f6f02343a4a6e3c24b1
patriknilsson1992@gmail.com <patriknilsson1992@gmail.com>
2018-08-28 21:55:43 +0200

    config webpack, react, scss

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "src/index.html"
+});
+
+const miniCssExtractPlugin = new MiniCssExtractPlugin({
+  filename: "[name].css",
+  chunkFilename: "[id].css"
+});
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "./dist")
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: "babel-loader", exclude: /node_modules/ },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin, miniCssExtractPlugin]
+};
