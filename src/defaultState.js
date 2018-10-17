4d2f76e55ecadb7baa15a93b50c202c252773ebc
patni1992 <patriknilsson1992@gmail.com>
2018-10-17 22:48:04 +0200

    update default state

--- src/defaultState.js
+++ src/defaultState.js
@@ -1,19 +1,19 @@
 const defaultState = {
   boards: {
-    10: {
-      id: 10,
+    "10": {
+      id: "10",
       title: "study",
       color: "red",
-      lists: [1337, 1338]
+      lists: ["1337", "1338"]
     },
-    11: {
-      id: 11,
+    "11": {
+      id: "11",
       title: "test",
       color: "orange",
       lists: []
     },
-    12: {
-      id: 12,
+    "12": {
+      id: "12",
       title: "Dev stuff",
       color: "blue",
       lists: []
@@ -25,13 +25,13 @@ const defaultState = {
     "3": { id: "3", title: "watch south park", color: "orange" }
   },
   lists: {
-    1337: {
-      id: 1337,
+    "1337": {
+      id: "1337",
       title: "Todo",
       cards: ["1", "2"]
     },
-    1338: {
-      id: 1338,
+    "1338": {
+      id: "1338",
       title: "In Progress",
       cards: ["3"]
     }
