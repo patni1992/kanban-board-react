55e564597ba876171cc15e41af90b6cf57085d01
Patrik <pn222en@student.lnu.se>
2018-10-31 17:45:17 +0100

    autoFocus on edit

--- src/components/Card/EditCard.js
+++ src/components/Card/EditCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const EditCard = props => (
-  <textarea defaultValue={props.text} style={{height: `${props.height}px`, width: "100%"}} id=""></textarea>
+  <textarea  autoFocus defaultValue={props.text} style={{height: `${props.height}px`, width: "100%"}} id=""></textarea>
 );
 
 export default EditCard;
